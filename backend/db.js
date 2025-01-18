376b3526f17e541ca7ff4487a1e4f63c57a4f1ec
Arvind Batham <arvind@Arvinds-MacBook-Air.local>
2025-01-19 03:28:37 +0530

    basic way to create db.js

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
 // connect mongodb url
-mongoose.connect("string to connect the server");
+mongoose.connect(
+  "mongodb+srv://arvindbatham60:mongopassword@cluster0.qph9o.mongodb.net/"
+);
 
 // create schema
 const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
+  username: String,
   password: String,
 });
 
 // create model
-const users = mongoose.model(userSchema);
+const User = mongoose.model("User", userSchema);
 
 // export the model
 module.exports = {
-  users,
+  User,
 };
