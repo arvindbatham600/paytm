38a9cb451cd222243a156c6c20073161f1ab1964
Arvind Batham <arvindbatham600@gmail.com>
2025-01-21 21:45:10 +0530

    added auth middleware

---
+++ backend/auth/AuthMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const AuthMiddleware = async (req, res, next) => {
+  //get the token from headers -->
+  const authorization = req.headers["authorization"];
+  // console.log("token", authorization.split(" ")[1]);
+  const token = authorization.split(" ")[1];
+  //   console.log("decoded value", decode.email);
+  try {
+    // jwt decode
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("decoded value", decode)
+      req.user = decode.email;
+      next(); // you can go ahead
+  } catch (e) {
+      res.status(500).send({
+          message: "Invalid Token",
+          success: false
+      })
+    console.log("error", e);
+  }
+};
+
+module.exports = AuthMiddleware;
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,11 +3,13 @@ const SignupValidation = require("../middlewares/SigninValidation")
 const SignupHander = require("../controllers/SignupHandler");
 const SigninValidation = require("../middlewares/SigninValidation");
 const SigninHandler = require("../controllers/SigninHandler");
+const AuthMiddleware = require("../auth/AuthMiddleware");
 const router = express.Router();
 
 // signup and signin router
 router.post("/signup", SignupValidation, SignupHander); 
-router.post("/signin", SigninValidation, SigninHandler )
+// router.post("/signin", SigninValidation, SigninHandler )
+router.post("/signin", AuthMiddleware )
 
 
 module.exports = router;
